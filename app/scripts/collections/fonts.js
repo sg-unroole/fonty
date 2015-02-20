3c71eee2ea3792aa39037b15ca0b2d333e5a8a08
Ilia <ilia.golod@softgravity.com>
2015-02-20 16:15:27 -0500

    put in a method in the font model to apply it to a specific jquery object

--- app/scripts/collections/fonts.js
+++ app/scripts/collections/fonts.js
@@ -5,11 +5,25 @@ define([
 function( Backbone, Font ) {
     'use strict';
 
+
 	/* Return a collection class definition */
 	return Backbone.Collection.extend({
 		initialize: function() {},
 
-		model: Font
-		
+		model: Font,
+
+		// options would be the weight, italic
+		setFont: function(jObject, options) {
+			//gets the font
+			var font_name = this.get("family");
+			WebFont.load({
+				google: {
+					families: [font_name]
+				}
+			});
+			jObject.css('font-family', font_name);
+		}
 	});
+
+
 });
