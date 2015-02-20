15374bb91afad0e567459cfc569d25713495982f
Anatoly Spektor <anatolijs.spektors@softgravity.com>
2015-02-20 16:32:05 -0500

    filter functionality

--- app/scripts/collections/fonts.js
+++ app/scripts/collections/fonts.js
@@ -8,8 +8,20 @@ function( Backbone, Font ) {
 	/* Return a collection class definition */
 	return Backbone.Collection.extend({
 		initialize: function() {},
+		model: Font,
 
-		model: Font
-		
+		randomize: function(filters) {
+			var filtered_collection = this.filterData(filters);
+			// get random from filtered collection
+
+		},
+
+		filterData: function(options) {
+			var filtered = this.models.filter(function(font) {
+   				 return _.contains(options, font.get("category"));
+			});
+
+			return filtered;
+		}
 	});
 });
